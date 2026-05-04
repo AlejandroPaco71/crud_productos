fd2feeb06fccaae6cc992ad37344de484539b79d
Alejandro Paco <alejandropacom2003@gmail.com>
2026-05-04 16:37:51 -0400

    Implementamos la funcion Insertar productos

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tutorial.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///tienda.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -26,10 +26,41 @@ def init_db():
     with app.app_context():
         db.create_all()
         print("Base de datos con tabla Product creado exitosamente")
+        
+#Funcion para Inseratar productos 
+def insert_product():
+    with app.app_context():
+        # Captura de datos
+        nombre_prod = input("Ingrese el nombre: ")
+        precio_prod = input("Ingrese el precio: ")
+        cantidad_prod = input("Ingrese el stock (opcional): ")
+
+        # Validamos si se proporcionaron todos los datos
+        if all([nombre_prod, precio_prod, cantidad_prod]):
+            nuevo_prod = Product(
+                name=nombre_prod, 
+                price=float(precio_prod), 
+                stock=int(cantidad_prod)
+            )
+            mensaje = "Producto registrado con exito"
+        else:
+            # Caso donde el stock queda vacío 
+            nuevo_prod = Product(
+                name=nombre_prod, 
+                price=float(precio_prod)
+            )
+            mensaje = "Producto guardado (stock predeterminado en 0)."
+
+        # Ejecución de persistencia en la BD
+        db.session.add(nuevo_prod)
+        db.session.commit()
+        print(mensaje)
+
 
 
         
 
 
 if __name__ == "__main__":
-    init_db()
+    #init_db()
+    insert_product()
